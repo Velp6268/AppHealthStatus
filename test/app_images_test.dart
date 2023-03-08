a7d7c16f05daa5c6f19be5ff47a335f6d1a42a9f
maksimnocevnyj <bagor6268@gmail.com>
2023-03-08 17:45:28 +0300

    process0.1

---
+++ test/app_images_test.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:health_status/resources/resources.dart';
+
+void main() {
+  test('app_images assets test', () {
+    expect(File(AppImages.man).existsSync(), true);
+  });
+}
