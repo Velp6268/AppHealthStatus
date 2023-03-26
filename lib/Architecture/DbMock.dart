b9e64eab3b8888672f29970665df85102819e9c6
Velp6268 <bagor6268@gmail.com>
2023-03-26 18:29:31 +0300

    update list users

--- lib/Architecture/DbMock.dart
+++ lib/Architecture/DbMock.dart
@@ -1,61 +1,70 @@
-import 'package:flutter/cupertino.dart';
 import 'package:health_status/Architecture/IDataSourse.dart';
-import 'package:health_status/Architecture/Result.dart';
 import 'package:health_status/Architecture/User.dart';
 import 'package:health_status/Theme/app_colors.dart';
 import 'package:health_status/resources/resources.dart';
-import 'package:collection/collection.dart';
+
 
 class DbMock implements IDataSource {
-  final _students = [
+  final students = [
     User(
         id: 1,
-        email: "bagor6268@gmail.com",
-        password: "1111",
         fullName: 'Ночевный М.Ю.',
+        email: 'bagor6268@gmail.com',
         imageName: AppImages.maks,
-        healthStatus: AppColors.statusHealthy),
+        healthStatus: AppColors.statusHealthy,
+        group: '32Д',
+    ),
     User(
         id: 2,
-        email: "Lazar@gmail.com",
-        password: "1111",
         fullName: 'Лазарев Н.С.',
+        email: 'lazurich@gmail.com',
         imageName: AppImages.nikita,
-        healthStatus: AppColors.statusHealthy),
+        healthStatus: AppColors.statusHealthy,
+        group: '32Д',
+    ),
+
     User(
         id: 3,
-        email: "Komord@gmail.com",
-        password: "1111",
         fullName: 'Комарденков Т.Д.',
+        email: 'kamardenkov@gmail.com',
         imageName: AppImages.tim,
-        healthStatus: AppColors.statusAnother),
+        healthStatus: AppColors.statusAnother,
+        group: '32Д',
+    ),
     User(
         id: 4,
-        email: "Plat@gmail.com",
-        password: "1111",
         fullName: 'Платонов В.И.',
+        email: 'Platonov@gmail.com',
         imageName: AppImages.vitalya,
-        healthStatus: AppColors.statusHealthy),
+        healthStatus: AppColors.statusHealthy,
+        group: '32Д',
+    ),
     User(
         id: 4,
-        email: "1111",
-        password: "1111",
         fullName: 'Неизвестный Н.Н.',
-        imageName: AppImages.city,
-        healthStatus: AppColors.statusUnHealthy),
+        email: 'noname228@gmail.com',
+        imageName: AppImages.man,
+        healthStatus: AppColors.statusUnHealthy,
+        group: '32Д',
+    ),
+
   ];
 
-  Result getByLoginAndPass(String login, String pass) {
-    try {
-      var result = _students.firstWhere(
-          (element) => element.email == login && element.password == pass);
-      return Result.success(result);
-    } on StateError {
-      return Result.error("Логин или пароль не верны");
+  @override
+  String statusHealthyText(User user ){
+
+    if(user.healthStatus == AppColors.statusHealthy){
+      return'Здооров';
     }
+    if(user.healthStatus == AppColors.statusUnHealthy){
+      return 'Не здоров';
+    }
+    if(user.healthStatus == AppColors.statusAnother){
+      return 'Отсуствует по обстоятельствам';
+    }
+
+    return '';
   }
-
-
 
   @override
   void delete(User user) {
@@ -71,7 +80,8 @@ class DbMock implements IDataSource {
   @override
   List<User> getAll() {
     // TODO: implement getAll
-    return _students;
+    return students;
+
   }
 
   @override
