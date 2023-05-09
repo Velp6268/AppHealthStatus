b278d47c63d3c784a4bbffc14e223f9ef3cc8eac
Velp6268 <bagor6268@gmail.com>
2023-05-09 20:35:43 +0300

    take json

--- lib/Architecture/profile/Models.dart
+++ lib/Architecture/profile/Models.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'Models.g.dart';
+@JsonSerializable()
+
 class ProfileUser{
   final int userId;
   String fullName;
@@ -5,4 +10,8 @@ class ProfileUser{
   String imageName;
   final String? textHealthStatus;
   ProfileUser({required this.userId, required this.fullName, required this.group, required this.imageName, required this.textHealthStatus});
+
+
+  factory ProfileUser.fromJson(Map<String, dynamic> json) => _$ProfileUserFromJson(json);
+  Map<String, dynamic> toJson() => _$ProfileUserToJson(this);
 }
